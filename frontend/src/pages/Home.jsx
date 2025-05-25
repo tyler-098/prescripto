7f78c79ff9f63dfb463cdf07bdf38215f6d0f689
piyush singh <128707961+tyler-098@users.noreply.github.com>
2025-05-25 18:28:44 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Header from '../components/Header'
 import SpecialityMenu from '../components/SpecialityMenu'
 import TopDoctors from '../components/TopDoctors'
-import Banner from '../components/banner'
+import Banner from '../components/Banner'
 import ArticlesSection from '../components/ArticleSelection'
 const Home = () => {
   return (
